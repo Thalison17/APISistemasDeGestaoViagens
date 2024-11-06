8f47a1681f00d0295a4968d39db516faabe2498a
Thalison Ribeiro - MULTIVIX/Serra-ES <thalison.ribeiro@multivix.edu.br>
2024-11-06 12:23:59 -0300

    Adicionando Controllers, Data e Repository

--- APISistemaGestaoViagens/obj/Debug/net8.0/APISistemaGestaoViagens.AssemblyInfo.cs
+++ APISistemaGestaoViagens/obj/Debug/net8.0/APISistemaGestaoViagens.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APISistemaGestaoViagens")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70ba76f5384bbc4f395e6b22f7353ceb51acfb27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5745b16c1e555d183ae20ec33481a0cea0588f5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("APISistemaGestaoViagens")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APISistemaGestaoViagens")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
