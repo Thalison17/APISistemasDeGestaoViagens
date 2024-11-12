7a444dc7e17cf6c3099b5e6a965a9902e8661e05
Thalison Vinicius <69252647+Thalison17@users.noreply.github.com>
2024-11-11 21:14:28 -0300

    Update ReservaDTO.cs

--- APISistemaGestaoViagens/Model/DTOs/ReservaDTO.cs
+++ APISistemaGestaoViagens/Model/DTOs/ReservaDTO.cs
@@ -1,9 +1,10 @@
-﻿namespace APISistemaGestaoViagens.Model.DTOs;
+namespace APISistemaGestaoViagens.Model.DTOs;
 
 public class ReservaDTO
 {
     public int ReservaId { get; set; }
     public int ClienteId { get; set; }
+    public int ViagemId { get; set; }
     public DateTime DataReserva { get; set; }
     public string StatusPagamento { get; set; }
     public string MetodoPagamento { get; set; }
